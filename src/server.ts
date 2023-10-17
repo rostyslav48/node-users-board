9273f68e93338b6498fdf1cde0cb6d6b7c70d949
Rostyslav Kravchuk <mr.shadov.05@gmail.com>
2023-10-17 10:24:09 +0300

    syntaxis correction

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,53 @@
 import express from 'express'
+import cors from 'cors'
 
 const PORT = 5000;
 const app = express();
 
+const users = [
+  { id: 1, name: 'Joe Biden', carColorId: 5 },
+  { id: 2, name: 'Elon Musk', carColorId: 4 },
+  { id: 3, name: 'Pan Roman', carColorId: 2 },
+];
+
+const colors = [
+  { id: 1, name: 'Black' },
+  { id: 2, name: 'DeepPink' },
+  { id: 3, name: 'Red' },
+  { id: 4, name: 'Aquamarine' },
+  { id: 5, name: 'Gold' },
+  { id: 6, name: 'YellowGreen' },
+  { id: 7, name: 'Yellow' },
+];
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+
 app.get('/', (req, res) => {
   res.end('hello world!')
+})
+
+app.get('/users', (req, res) => {
+  res.send(users)
+})
+
+app.get('/users/:id', (req, res) => {
+  const {id} = req.params;
+
+  const searchedUser = users.find(user => user.id === +id)
+
+  if (!searchedUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(searchedUser)
+})
+
+app.get('/colors', (req, res) => {
+  res.send(colors)
 })
 
 app.listen(PORT, () => {
